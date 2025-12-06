6358fe88d4d1e8d3302b1138532ec80e2007135f
Tuff Developer <dev@tuff.lang>
2025-12-06 18:20:42 -0500

    test: switch to chai expect assertions

--- tests/interpret.test.ts
+++ tests/interpret.test.ts
@@ -1,16 +1,10 @@
-const assert = require("assert").strict;
-const { interpret: interpretFn } = require("../src/interpret");
+const { expect: chaiExpect } = require('chai')
+const { interpret: interpretFn } = require('../src/interpret')
 
-describe("interpret", function () {
-  it("throws a stub error", function () {
-    try {
-      interpretFn("hello");
-      throw new Error("expected interpret to throw");
-    } catch (err) {
-      assert(err instanceof Error);
-      assert.equal((err as Error).message, "interpret: stub not implemented");
-    }
-  });
-});
+describe('interpret', function () {
+  it('throws a stub error', function () {
+    chaiExpect(() => interpretFn('hello')).to.throw('interpret: stub not implemented')
+  })
+})
 
-module.exports = {};
+module.exports = {}
