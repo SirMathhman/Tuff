81d95df881485bc7f074cc8691ec669072359706
Tuff Developer <dev@tuff.lang>
2025-12-06 17:49:37 -0500

    Delete

--- src/test/windows/test_method.rs
+++
@@ -1,9 +0,0 @@
-use tuff::interpret;
-
-#[test]
-fn method_call_on_constructor() {
-    let result = interpret(
-        r#"fn Point(x : I32, y : I32) => {fn manhattan() => x + y; this}; let p : Point = Point(3, 4); p.manhattan()"#,
-    );
-    assert_eq!(result, Ok("7".to_string()));
-}
--- src/test/windows/test_struct_generics.rs
+++
@@ -1,8 +0,0 @@
-use tuff::interpret;
-
-#[test]
-fn generic_struct_construct_access() {
-    let res = interpret("struct Wrapper<T> { value : T } Wrapper<I32> { 100 }.value");
-    eprintln!("single test result: {:?}", res);
-    assert_eq!(res, Ok("100".to_string()));
-}
