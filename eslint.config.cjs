a6128473e2d2cccb9c60964a93f35dbe41a39c7d
Tuff Developer <dev@tuff.lang>
2025-12-08 19:50:36 -0500

    chore(eslint): remove legacy .eslintrc.cjs and .eslintignore (migrated to eslint.config.cjs)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,29 +2,34 @@ module.exports = [
   // ignore common files/dirs
   {
     ignores: [
-      'node_modules/',
-      'dist/',
-      'build/',
-      'bun.lock',
-      '/*.log',
-      'coverage/',
-      '.turbo/',
-      '.cache/',
+      "node_modules/",
+      "dist/",
+      "build/",
+      "bun.lock",
+      "/*.log",
+      "coverage/",
+      ".turbo/",
+      ".cache/",
     ],
   },
 
   // TypeScript + JS rules for source files
   {
-    files: ['**/*.{ts,tsx,js}'],
+    files: ["**/*.{ts,tsx,js}"],
     languageOptions: {
-      parser: require('@typescript-eslint/parser'),
-      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: { ecmaVersion: "latest", sourceType: "module" },
     },
-    plugins: { '@typescript-eslint': require('@typescript-eslint/eslint-plugin') },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
     rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      complexity: ['warn', { max: 15 }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      complexity: ["warn", { max: 15 }],
     },
   },
-]
+];
