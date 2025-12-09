84f6148833bd571988375aa26acb038884446a41
Tuff Developer <dev@tuff.lang>
2025-12-08 19:49:57 -0500

    chore(eslint): migrate .eslintrc.cjs to eslint.config.cjs (flat config)

---
+++ eslint.config.cjs
@@ -0,0 +1,30 @@
+module.exports = [
+  // ignore common files/dirs
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      'bun.lock',
+      '/*.log',
+      'coverage/',
+      '.turbo/',
+      '.cache/',
+    ],
+  },
+
+  // TypeScript + JS rules for source files
+  {
+    files: ['**/*.{ts,tsx,js}'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    },
+    plugins: { '@typescript-eslint': require('@typescript-eslint/eslint-plugin') },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      complexity: ['warn', { max: 15 }],
+    },
+  },
+]
