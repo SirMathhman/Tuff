21a4d7fcc4c703ab1b7ba9b1e9806217423ccac9
Tuff Developer <dev@tuff.lang>
2025-12-11 10:21:03 -0500

    Double copy

--- src/main/java/tuff/Main0.java
+++ src/main/java/tuff/Main0.java
@@ -4,27 +4,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Function;
 
-public class Main {
+public class Main0 {
 	public static void main(String[] args) {
 		try {
-			run(Paths.get(".", "src", "main", "java", "tuff", "Main.java"),
-					Paths.get(".", "src", "main", "tuff", "tuff", "Main.tuff"));
+			run(Paths.get(".", "src", "main", "java", "tuff", "Main0.java"),
+					Paths.get(".", "src", "main", "tuff", "tuff", "Main0.tuff"), Main0::compile);
 
-			run(Paths.get(".", "src", "main", "tuff", "tuff", "Main.tuff"),
-					Paths.get(".", "src", "main", "java", "tuff", "Main0.java"));
+			run(Paths.get(".", "src", "main", "tuff", "tuff", "Main0.tuff"),
+					Paths.get(".", "src", "main", "java", "tuff", "Main00.java"), input -> getReplace(input));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	private static void run(Path source, Path target) throws IOException {
+	private static String getReplace(String input) {
+		return input.replace("Main0", "Main00");
+	}
+
+	private static void run(Path source, Path target, Function<String, String> compiler) throws IOException {
 		final var input = Files.readString(source);
 		final var targetDirectory = target.getParent();
 		if (!Files.exists(targetDirectory)) {
 			Files.createDirectories(targetDirectory);
 		}
 
-		Files.writeString(target, input);
+		Files.writeString(target, compiler.apply(input));
+	}
+
+	private static String compile(String input) {
+		return input;
 	}
 }
