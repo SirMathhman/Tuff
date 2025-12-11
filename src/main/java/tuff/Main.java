4b7a04181eee023a00060cf91e11fca40f3ff358
Tuff Developer <dev@tuff.lang>
2025-12-11 00:06:12 -0500

    Update Main class to include compile method for processing input; modify .gitignore to exclude target directory

--- src/main/java/tuff/Main.java
+++ src/main/java/tuff/Main.java
@@ -3,6 +3,8 @@ package tuff;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,9 +15,40 @@ public class Main {
 			if (!Files.exists(targetDirectory)) {
 				Files.createDirectories(targetDirectory);
 			}
-			Files.writeString(target, input);
+			Files.writeString(target, compile(input));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private static String compile(String input) {
+		final var segments = new ArrayList<String>();
+		var buffer = new StringBuffer();
+		for (var i = 0; i < input.length(); i++) {
+			final var c = input.charAt(i);
+			buffer.append(c);
+
+			if (c == ';') {
+				segments.add(buffer.toString());
+				buffer = new StringBuffer();
+			}
+		}
+		segments.add(buffer.toString());
+
+		return segments
+				.stream()
+				.map(String::strip)
+				.filter(slice -> !slice.isEmpty())
+				.map(Main::compileRootSegment)
+				.collect(Collectors.joining());
+	}
+
+	private static String compileRootSegment(String input) {
+		final var stripped = input.strip();
+		if (stripped.startsWith("package ")) {
+			return "";
+		}
+
+		return stripped;
 	}
 }
--- src/main/ts/tuff/Main.ts
+++ src/main/ts/tuff/Main.ts
@@ -1,21 +1,27 @@
-package tuff;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-public class Main {
+import java.io.IOException;import java.nio.file.Files;import java.nio.file.Paths;import java.util.ArrayList;import java.util.stream.Collectors;public class Main {
 	public static void main(String[] args) {
 		try {
-			final var input = Files.readString(Paths.get(".", "src", "main", "java", "tuff", "Main.java"));
-			final var target = Paths.get(".", "src", "main", "ts", "tuff", "Main.ts");
-			final var targetDirectory = target.getParent();
-			if (!Files.exists(targetDirectory)) {
-				Files.createDirectories(targetDirectory);
-			}
-			Files.writeString(target, input);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+			final var input = Files.readString(Paths.get(".", "src", "main", "java", "tuff", "Main.java"));final var target = Paths.get(".", "src", "main", "ts", "tuff", "Main.ts");final var targetDirectory = target.getParent();if (!Files.exists(targetDirectory)) {
+				Files.createDirectories(targetDirectory);}
+			Files.writeString(target, compile(input));} catch (IOException e) {
+			throw new RuntimeException(e);}
 	}
+
+	private static String compile(String input) {
+		final var segments = new ArrayList<String>();var buffer = new StringBuffer();for (var i = 0;i < input.length();i++) {
+			final var c = input.charAt(i);buffer.append(c);if (c == ';') {
+				segments.add(buffer.toString());buffer = new StringBuffer();}
+		}
+		segments.add(buffer.toString());return segments
+				.stream()
+				.map(String::strip)
+				.filter(slice -> !slice.isEmpty())
+				.map(Main::compileRootSegment)
+				.collect(Collectors.joining());}
+
+	private static String compileRootSegment(String input) {
+		final var stripped = input.strip();if (stripped.startsWith("package ")) {
+			return "";}
+
+		return stripped;}
 }
