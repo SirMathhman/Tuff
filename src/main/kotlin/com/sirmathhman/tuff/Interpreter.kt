166b5664bf68e6ca24a358e8ef0f02d17b630392
Tuff Developer <dev@tuff.lang>
2026-01-05 09:07:03 -0500

    Add stubbed interpret(Int) function and Gradle test setup (JUnit 5); add tests

---
+++ src/main/kotlin/com/sirmathhman/tuff/Interpreter.kt
@@ -0,0 +1,9 @@
+package com.sirmathhman.tuff
+
+/**
+ * Interpret the given input and return an Int result.
+ * Currently stubbed to throw an error until implemented.
+ */
+fun interpret(input: String): Int {
+    throw UnsupportedOperationException("interpret is not implemented yet")
+}
