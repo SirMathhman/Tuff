78588c73d81451e20d64cfec345c3e673521d19a
Tuff Developer <dev@tuff.lang>
2026-01-05 19:19:09 -0500

    Allow omission of braces in simple ifs: update .clang-tidy and remove braces in single-statement ifs

--- src/interpret.c
+++ src/interpret.c
@@ -5,26 +5,22 @@
 #include <stdlib.h>
 
 int interpret(const char *s) {
-	if (s == NULL) {
+	if (s == NULL)
 		return -1;
-	}
 	errno = 0;
 	char *end = NULL;
 	long val = strtol(s, &end, 10);
 	/* No digits were found */
-	if (end == s) {
+	if (end == s)
 		return -1;
-	}
 	/* Allow trailing whitespace only */
 	while (*end != '\0' && isspace((unsigned char)*end)) {
 		end++;
 	}
-	if (*end != '\0') {
+	if (*end != '\0')
 		return -1;
-	}
 	/* Range/overflow check */
-	if (errno == ERANGE || val < INT_MIN || val > INT_MAX) {
+	if (errno == ERANGE || val < INT_MIN || val > INT_MAX)
 		return -1;
-	}
 	return (int)val;
 }
