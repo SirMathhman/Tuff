49b05fb14f48fd7ec2c06cffcb1938c196a4095c
Tuff Developer <dev@tuff.lang>
2026-01-09 17:08:53 -0500

    Delete

--- src/runtime/global.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Env } from "./env";
-
-declare global {
-  // Exposed so `eval.ts` can call interpret without circular imports.
-  // (Set in `src/interpret.ts`.)
-  var interpret: ((_input: string, _env?: Env) => number) | undefined;
-}
-
-export {};
