94d451b3e3520b43c1a4b11632519cc5a004d593
Tuff Developer <dev@tuff.lang>
2026-01-10 16:38:58 -0500

    chore(lint): add no-restricted-syntax rule to ban RegExp.test()

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,15 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
     // Keep minimal rules for now; we can tighten these later
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    // Ban RegExp.test() using no-restricted-syntax
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "CallExpression[callee.type='MemberExpression'][callee.property.name='test']",
+        message: "RegExp.test() is banned. Use RegExp.exec() or string.match() instead."
+      }
+    ]
   }
-}
+};
