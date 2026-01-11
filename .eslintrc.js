03b7264a152156c2c413e57126242c474d37f15f
Tuff Developer <dev@tuff.lang>
2026-01-11 15:18:50 -0500

    chore: add ESLint with rule banning regex usage; add lint script

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    // Ban regex literals and RegExp usage
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "Literal[regex]",
+        message: 'Regex literals are banned. Use alternative parsing methods instead.',
+      },
+      {
+        selector: "NewExpression[callee.name='RegExp']",
+        message: 'Using RegExp constructor is banned. Use alternative parsing methods instead.',
+      },
+      {
+        selector: "CallExpression[callee.name='RegExp']",
+        message: 'Calling RegExp is banned. Use alternative parsing methods instead.',
+      },
+    ],
+  },
+};
