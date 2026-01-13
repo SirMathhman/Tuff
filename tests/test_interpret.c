b7869a778a0a8a4a04b87b7073f8eb1e976679fc
Tuff Developer <dev@tuff.lang>
2026-01-13 16:08:29 -0500

    Test multiple addition terms: 1 + 2 + 3

--- tests/test_interpret.c
+++ tests/test_interpret.c
@@ -36,6 +36,7 @@ int main(void)
 	assert(interpret("1U8 + 2U8") == 3);
 	assert(interpret("1U8 + 2U16") == 3);
 	assert(interpret("1U8 + 2") == 3);
+	assert(interpret("1 + 2 + 3") == 6);
 	assert(interpret("100 + 200") == 300);
 
 	return 0;
