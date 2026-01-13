0f70552bc6ad9006d5d0b73057bc98aa0ac9df25
Tuff Developer <dev@tuff.lang>
2026-01-13 07:29:06 -0500

    feat: add stubbed interpret(String) method and unit test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,4 +5,12 @@ public class App {
 		System.out.println("Hello from Tuff!");
 		System.out.println("Java version: " + System.getProperty("java.version"));
 	}
+
+	/**
+	 * Interpret the given string and return an int.
+	 * Stubbed implementation returning 0.
+	 */
+	public static int interpret(String input) {
+		return 0;
+	}
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,4 +9,9 @@ public class AppTest {
 	void simpleTest() {
 		assertTrue(true, "Sanity check");
 	}
+
+	@Test
+	void interpretReturnsStub() {
+		assertEquals(0, App.interpret("any input"));
+	}
 }
