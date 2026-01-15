ed895568a2ce97bf7f8fcd46403dc5b4add7f729
Tuff Developer <dev@tuff.lang>
2026-01-15 17:33:26 -0500

    Add test case and setup pre-commit with cargo test and clippy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[allow(dead_code)]
+fn interpret(input: &str) -> i32 {
+    input.parse::<i32>().ok().map_or(0, |x| x)
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpret_100() {
+        assert_eq!(interpret("100"), 100);
+    }
 }
