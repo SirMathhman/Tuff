9d1b8e60c987fce386c3c36a094c84bf09259d95
Tuff Developer <dev@tuff.lang>
2026-01-15 17:35:09 -0500

    Add support for type suffixes like U8 in interpret function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[allow(dead_code)]
 fn interpret(input: &str) -> i32 {
-    input.parse::<i32>().ok().map_or(0, |x| x)
+    let trimmed = input
+        .chars()
+        .take_while(|c| c.is_ascii_digit())
+        .collect::<String>();
+    trimmed.parse::<i32>().ok().map_or(0, |x| x)
 }
 
 fn main() {
@@ -14,5 +18,10 @@ mod tests {
     #[test]
     fn test_interpret_100() {
         assert_eq!(interpret("100"), 100);
+    }
+
+    #[test]
+    fn test_interpret_100u8() {
+        assert_eq!(interpret("100U8"), 100);
     }
 }
