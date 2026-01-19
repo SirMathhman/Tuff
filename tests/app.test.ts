ae259776f17b868667650dc64bca50ca1204bfbc
Test User <test@example.com>
2026-01-19 00:01:58 -0500

    Revert "Prevent non-halting programs from hanging tests"
    This reverts commit db8e99d3f998ae0f85c5ca89a727e5ead4edbeaf.

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,31 +1,8 @@
 import { run } from "../src/app";
-import { execute, Operation, Variant, type Instruction } from "../src/execute";
 
 describe("The application", () => {
   testSimplest("should run the simplest program possible", "", 0);
   testSimplest("should run with an int", "0", 0);
-});
-
-describe("The VM", () => {
-  test("should not run forever if program never halts", () => {
-    const program: Instruction[] = [
-      {
-        operation: Operation.Jump,
-        variant: Variant.Constant,
-        firstOperand: 0,
-        secondOperand: 0,
-      },
-    ];
-
-    expect(() =>
-      execute(
-        program,
-        () => 0,
-        () => {},
-        { maxSteps: 25 },
-      ),
-    ).toThrow(/did not halt/i);
-  });
 });
 
 function testSimplest(message: string, source: string, exitCode: number) {
