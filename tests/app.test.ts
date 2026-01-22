e758bd8d1f0993361a4b22c7eaf19d986ad6acde
Test User <test@example.com>
2026-01-21 20:30:34 -0500

    Add compile helper functions for testing VM execution

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,24 @@
 // Our first compile test, the VM should shut down instantly
 
-import { run } from "../src/app";
+import { compile, run } from "../src/app";
+
+// Test helpers
+function assertValid(source: string, expected: number, ...stdIn: number[]) {
+  const result = run(source, stdIn);
+  if (!result.ok) {
+    expect(result.error).toBeUndefined();
+  } else {
+    expect(result.value).toBe(expected);
+  }
+}
+
+// For assert invalid, we just have to make sure compilation fails, we don't have to run it in the VM
+function assertInvalid(source: string) {
+  let result = compile(source);
+  if (result.ok) {
+		expect(result.value).toBeUndefined();
+  }
+}
 
 describe("The application", () => {
   it("should execute a simple program that halts immediately", () => {
