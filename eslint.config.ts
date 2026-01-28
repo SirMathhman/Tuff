371f782d2b28ee4a785b31bee6dd8683e2b58b74
Test User <test@example.com>
2026-01-28 09:21:45 -0500

    ESlint config

---
+++ eslint.config.ts
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["node_modules/", "dist/", ".husky/"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+    },
+  },
+];
