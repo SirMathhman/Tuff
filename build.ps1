fc490df1a17d04cfdb83ae487f24f04a605b78d5
SirMathhman <sirmathhman@gmail.com>
2026-01-31 20:20:24 -0500

    Nuke

--- build.ps1
+++
@@ -1,1 +0,0 @@
-bun main.js
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  collectCoverageFrom: ["**/*.js", "!node_modules/**", "!coverage/**"],
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
-};
