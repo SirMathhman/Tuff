842b0825fcc267b0d8a1e13530d2ca6dc14be63e
SirMathhman <sirmathhman@gmail.com>
2026-02-02 11:08:24 -0500

    Nuke

--- src/ast/nodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function node(type, fields) {
-  return { type, ...fields };
-}
-
-module.exports = {
-  node,
-};
--- src/lower/desugar.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function desugar(program) {
-  return program;
-}
-
-module.exports = { desugar };
