65dd539d0cbd699738ad3679d7d1f9fcac174083
SirMathhman <sirmathhman@gmail.com>
2026-02-02 11:17:10 -0500

    chore: add ESLint rule to ban template strings

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      parser: tsParser
+    },
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "Literal[regex]",
+          message: "Regex literals are not allowed"
+        },
+        {
+          selector: "TemplateLiteral",
+          message: "Template strings are not allowed"
+        }
+      ]
+    }
+  }
+];
