ba7891003adc4d9a3951be7827a3a2cc2acf3281
SirMathhman <sirmathhman@gmail.com>
2026-02-02 11:12:49 -0500

    feat: implement interpret function and add initial tests for interpreter

---
+++ tests/index.test.ts
@@ -0,0 +1,15 @@
+import { interpret } from "../src";
+
+function expectValid(source: string, exitCode: number) {
+  expect(interpret(source)).toBe(exitCode);
+}
+
+// function expectInvalid(source: string) {
+//   expect(() => interpret(source)).toThrow();
+// }
+
+describe("The interpreter can interpret", () => {
+  test("an empty program", () => {
+    expectValid("", 0);
+  });
+});
