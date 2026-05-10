5353e1850844a1c13e0e0ac6ba8f3b73550b39a6
SirMathhman <sirmathhman@gmail.com>
2026-05-09 21:22:48 -0400

    feat: enhance Stop.ps1 to include success messages for test and lint processes

--- compiler/Stop.ps1
+++ compiler/Stop.ps1
@@ -1,1 +1,7 @@
-npm run test; if ($LASTEXITCODE -ne 0) { exit 1 }
+npm run test; 
+if ($LASTEXITCODE -ne 0) { exit 1 }
+Write-Host "All tests passed.";
+
+npm run lint;
+if ($LASTEXITCODE -ne 0) { exit 1 }
+Write-Host "Linting passed.";
