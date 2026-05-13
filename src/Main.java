369a8993d065172f7d0f0761855270c5dc318acb
SirMathhman <sirmathhman@gmail.com>
2026-05-13 11:50:16 -0400

    feat: implement segment-based compilation logic in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
+
+	public static final String LINE_SEPARATOR = System.lineSeparator();
+
 	public static void main(String[] args) {
 		try {
 			final var source = Paths.get(".", "src", "Main.java");
@@ -14,6 +19,34 @@ public class Main {
 	}
 
 	private static String compile(String input) throws IOException {
+		final var segments = new ArrayList<String>();
+		final var buffer = new StringBuilder();
+		for (var i = 0; i < input.length(); i++) {
+			final var c = input.charAt(i);
+			buffer.append(c);
+			if (c == ';') {
+				segments.add(buffer.toString());
+				buffer.setLength(0);
+			}
+		}
+
+		return segments
+				.stream()
+				.map(Main::compileRootSegment)
+				.map(slice -> slice + LINE_SEPARATOR)
+				.collect(Collectors.joining());
+	}
+
+	private static String compileRootSegment(String input) {
+		final var stripped = input.strip();
+		if (stripped.isEmpty()) {
+			return "";
+		}
+
+		return wrap(stripped);
+	}
+
+	private static String wrap(String input) {
 		final var replaced = input.replace("/*", "start").replace("*/", "end");
 		return "/*" + replaced + "*/";
 	}
