8670cc36b8a126e531d1acdd0bcccc084640ec36
SirMathhman <sirmathhman@gmail.com>
2026-05-13 11:44:45 -0400

    feat: add Main.js generation from Main.java with basic compilation logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 	public static void main(String[] args) {
+		try {
+			final var source = Paths.get(".", "src", "Main.java");
+			Files.writeString(source.resolveSibling("Main.js"), compile(Files.readString(source)));
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
+	}
 
+	private static String compile(String input) throws IOException {
+		final var replaced = input.replace("/*", "start").replace("*/", "end");
+		return "/*" + replaced + "*/";
 	}
 }
