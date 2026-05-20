b6a07b6bf204d91d4ee62df1a119826260a5eaaa
SirMathhman <sirmathhman@gmail.com>
2026-05-19 21:33:26 -0400

    Nuke

--- compiler/src/index.js
+++
@@ -1,8 +0,0 @@
-export function compile(source) {
-  if (source.trim().length === 0) return "return 0;";
-
-  const hasReadU8 = source.indexOf("read<U8>()") !== -1;
-  if (hasReadU8 && !source.includes("let") && !source.includes("const")) {
-    return "return parseInt(stdIn, 10) & 255;";
-  }
-}
