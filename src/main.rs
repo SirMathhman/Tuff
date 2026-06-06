ce73ec6d31ca0e4be9e57866530f071e2aee968c
SirMathhman <sirmathhman@gmail.com>
2026-06-06 12:42:22 -0400

    feat: initialize project with Cargo configuration and main structure

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn compile_tuff_to_c() {
+    // TODO: Takes in Tuff source and spits out C source.
+}
+
+fn execute_tuff() {
+    /*
+    Two parameters. Tuff source, and stdIn.
+    Returns the exit code.
+
+    1) Compile tuff source with C.
+    2) Compile C source to .exe using clang.
+    3) Run the .exe with stdIn. Don't pass stdIn to compile!
+    4) Return the generated exit code.
+    */
+}
+
+fn main() {
+    println!("Hello, world!");
+}
