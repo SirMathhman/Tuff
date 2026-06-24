e51b5f766c9054dc3c943cbd045bbfb61e04ed60
SirMathhman <sirmathhman@gmail.com>
2026-06-24 13:34:31 -0400

    fix: update file paths in index.js for correct module resolution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import { compileTuffToJS } from "./lib.js";
 
-const source = fs.readFileSync("../main.tuff", "utf-8");
+const source = fs.readFileSync("./main.tuff", "utf-8");
 const target = compileTuffToJS(source);
 if (target.variant === "err") {
   console.error(target.error);
@@ -9,4 +9,4 @@ if (target.variant === "err") {
 }
 
 const wrapInExit = "process.exit(" + target.value + ");";
-fs.writeFileSync("../main.js", wrapInExit, "utf-8");
+fs.writeFileSync("./main.js", wrapInExit, "utf-8");
