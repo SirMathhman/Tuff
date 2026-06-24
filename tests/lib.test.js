b776bed49282286e6a08a34041a49f3a0a4bef8b
SirMathhman <sirmathhman@gmail.com>
2026-06-24 13:17:59 -0400

    feat: restructure Tuff compiler by implementing a new lexer/parser/codegen pipeline and adding tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { compileTuffToJS } from "./lib";
+import { compileTuffToJS } from "../src/lib.js";
 
 function expectValid(source, stdIn, expectedExitCode) {
   const result = compileTuffToJS(source);
