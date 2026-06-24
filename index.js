106569c793d8ffd0afd79b2684fbf4a1f65c0615
SirMathhman <sirmathhman@gmail.com>
2026-06-24 12:37:16 -0400

    feat: implement error handling in compileTuffToJS and add hooks configuration

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ export function compileTuffToJS(source) {
   throw new Error("Invalid source: " + source);
 }
 
-// This might be broken a little
 const source = await fs.readFile("./main.tuff", "utf-8");
 const target = compileTuffToJS(source);
 await fs.writeFile("./main.js", target, "utf-8");
