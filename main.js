a62da71b1bf030e02bdec574b0541459043f8ee8
SirMathhman <sirmathhman@gmail.com>
2026-06-26 13:23:57 -0400

    Nuke

--- main.js
+++
@@ -1,7 +0,0 @@
-process.exit((() => {var _ctx = {};
-const tokens = stdIn.split(/\s+/).map(t => parseInt(t, 10));
-function compileTuffToJS(source) {  }
-_ctx.compileTuffToJS = compileTuffToJS;
-_ctx.source = _ctx.readFileSync(_ctx.fs, "./main.tuff", "utf-8");
-_ctx.target = _ctx.compileTuffToJS(_ctx.source);
-return 0;})());
