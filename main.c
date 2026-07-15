936d20ae4d0edc3dccffa7e15c3d591d23f50db9
SirMathhman <sirmathhman@gmail.com>
2026-07-15 12:21:45 -0400

    Hmmm

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+typedef struct {
+		const char * name;
+		int type_params;
+		const char * fields_str;
+} GenericStructTemplate;
+
+
 int main() {
+	
 	return 0;
 }
