fdbcf4a62fb2aec56c23ea48d8e8c1072f5d70a6
SirMathhman <sirmathhman@gmail.com>
2026-07-23 18:14:16 -0400

    More output fixing

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,10 +2,11 @@ import * as fs from "fs/promises";
 import { compile } from "./compile";
 
 async function run() {
-  const source = await fs.readFile("./src/main/tuff/lib.tuff");
+  const source = await fs.readFile("./src/main/tuff/lib.tuff", "utf-8");
   const target = compile(source);
   if (target.ok) {
-    await fs.writeFile("./dist/lib.js", target.value);
+    const content = "const __args__ = process.argv;\nprocess.exit(() => {\n" + target.value + "\n});";
+    await fs.writeFile("./dist/lib.js", content, "utf-8");
   } else {
     console.error(target.error);
   }
