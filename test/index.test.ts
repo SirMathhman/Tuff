047f4593eb886927a956c19ec543d30bd430b7d6
SirMathhman <sirmathhman@gmail.com>
2026-07-23 17:40:33 -0400

    Nuke

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "bun:test";
-import { hello } from "../src/index";
-
-describe("hello", () => {
-  it("should return a greeting", () => {
-    expect(hello()).toBe("Hello, Tuff!");
-  });
-});
