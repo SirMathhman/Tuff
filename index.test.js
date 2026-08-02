1b0c9ef5fc14fa195436aa9bd8cfcf9c786fb30e
SirMathhman <sirmathhman@gmail.com>
2026-08-01 22:59:31 -0400

    Nuke

--- index.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "bun:test";
-import { compile } from ".";
-
-function execute(source, args) {
-  const generated = compile("in let args : &[&Str]; " + source);
-  return new Function("args", generated + " main()");
-}
-
