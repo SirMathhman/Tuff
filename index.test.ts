e63ec1445a1321851dc3eddf330498f50c16a17d
SirMathhman <sirmathhman@gmail.com>
2026-08-03 13:25:22 -0400

    Add test for chained else if

--- index.test.ts
+++ index.test.ts
@@ -112,3 +112,7 @@ test("evaluate not equal", () => {
 test("evaluate if expression with else", () => {
   expect(evaluate("let x = if (true) 2 else 3; x")).toBe(2);
 });
+
+test("evaluate chained else if", () => {
+  expect(evaluate("let x = if (false) 2 else if (false) 3 else 4; x")).toBe(4);
+});
