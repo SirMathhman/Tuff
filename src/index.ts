19e656c90d9380864c997b66ed367330677d78dd
SirMathhman <sirmathhman@gmail.com>
2026-08-02 20:14:08 -0400

    Nuke

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { lex } from "./lexer.ts";
-import { parse } from "./parser.ts";
-import { resolve } from "./resolve.ts";
-import { codegen } from "./codegen.ts";
-
-export function compileTuffToJS(source: string): string {
-  return codegen(resolve(parse(lex(source))));
-}
