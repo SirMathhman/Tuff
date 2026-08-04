3a07edf14a3dc69ebb327f42c6614f0b7988af16
SirMathhman <sirmathhman@gmail.com>
2026-08-03 21:17:07 -0400

    Test chained else if

--- index.test.ts
+++ index.test.ts
@@ -109,3 +109,7 @@ test("evaluate if expression", () => {
   expect(evaluate("let x = if (false) 2 else 3; x")).toBe(3);
 });
 
+test("evaluate chained else if", () => {
+  expect(evaluate("let x = if (false) 2 else if (false) 3 else 4; x")).toBe(4);
+});
+
