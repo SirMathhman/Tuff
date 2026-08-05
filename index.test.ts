69e2e85b16951003158eca79dbcd46d610d104fa
SirMathhman <sirmathhman@gmail.com>
2026-08-04 22:39:17 -0400

    add test for evaluate("1") => 1 and implement number parsing

--- index.test.ts
+++ index.test.ts
@@ -8,3 +8,7 @@ test('evaluate("") => 0', () => {
 test('evaluate(" ") => 0', () => {
   expect(evaluate(" ")).toBe(0);
 });
+
+test('evaluate("1") => 1', () => {
+  expect(evaluate("1")).toBe(1);
+});
