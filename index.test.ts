bfc499921463559d2fd9f8142e112f72b1c16d04
SirMathhman <sirmathhman@gmail.com>
2026-08-04 20:45:10 -0400

    test: add evaluate("(2 + 3) * (1 + 3)") => 20

--- index.test.ts
+++ index.test.ts
@@ -37,3 +37,7 @@ test('evaluate("(2 + 3) * 4") => 20', () => {
   expect(evaluate("(2 + 3) * 4")).toBe(20);
 });
 
+test('evaluate("(2 + 3) * (1 + 3)") => 20', () => {
+  expect(evaluate("(2 + 3) * (1 + 3)")).toBe(20);
+});
+
