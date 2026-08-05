cf9a1c4f53811332c019fda32043a2950a355f1c
SirMathhman <sirmathhman@gmail.com>
2026-08-04 22:37:32 -0400

    Nuke

--- index.ts
+++ index.ts
@@ -1,11 +1,3 @@
-import { tokenize } from "./src/tokenize";
-import { parse } from "./src/parse";
-import { evaluateExpr } from "./src/evaluate";
-
 export function evaluate(source: string): number {
-  const trimmed = source.trim();
-  if (!trimmed) return 0;
-  const tokens = tokenize(trimmed);
-  const ast = parse(tokens);
-  return evaluateExpr(ast);
+  return 0;
 }
