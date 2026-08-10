c9307ba562bd278db8675867fc9f547adc019125
SirMathhman <sirmathhman@gmail.com>
2026-08-10 12:37:43 -0400

    Simplify compileTuffToJS by removing redundant numeric branch

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ export function compileTuffToJS(source: string) {
   if (source.includes("#")) {
     throw new Error("Source contains '#' which is not valid Tuff syntax");
   }
-  if (/^\d+$/.test(source)) {
-    return "process.exit(" + source + ")";
-  }
   if (/^[\d\s\+\-\*\/\(\)]+$/.test(source)) {
     return "process.exit(" + source + ")";
   }
