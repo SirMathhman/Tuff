b2a463629fdad357cc4e9e248b12f7c322882498
SirMathhman <sirmathhman@gmail.com>
2026-08-10 12:38:36 -0400

    Replace default case

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 export function compileTuffToJS(source: string) {
-  if (source.includes("#")) {
-    throw new Error("Source contains '#' which is not valid Tuff syntax");
-  }
   if (/^[\d\s\+\-\*\/\(\)]+$/.test(source)) {
     return "process.exit(" + source + ")";
   }
-  return "";
+  throw new Error("Source contains invalid Tuff syntax");
 }
