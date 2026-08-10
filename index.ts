5b79de5eb3d6d3fb5c02bf28f4c6a624d9c3b6b7
SirMathhman <sirmathhman@gmail.com>
2026-08-10 12:35:40 -0400

    Improve error message for invalid Tuff source

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export function compileTuffToJS(source: string) {
   if (source.includes("#")) {
-    throw new Error("Invalid source");
+    throw new Error("Source contains '#' which is not valid Tuff syntax");
   }
   return "";
 }
