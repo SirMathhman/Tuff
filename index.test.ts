6a4a48a17bc1e0ececd70e21a1ef2d0339f07961
SirMathhman <sirmathhman@gmail.com>
2026-08-09 22:13:07 -0400

    Scaffold

---
+++ index.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "bun:test";
+import { compileTuffToJS } from ".";
+
+function executeTuff(tuffSource: string, args: string[]): number {
+  const generatedJS = compileTuffToJS("in let args; " + tuffSource);
+  
+  let exitCode = 0;
+  const process = {
+    exit(value: number) {
+      exitCode = value;
+    },
+  };
+
+  new Function("process", "args", generatedJS)(process, args);
+  return exitCode;
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-console.log("Hello via Bun!");
+export function compileTuffToJS(tuffSource: string): string {
+  throw new Error("TODO");
+}
