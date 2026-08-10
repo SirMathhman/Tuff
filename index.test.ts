05d83f59499d4f435a8840e5d0fd35ae20f8f2d8
SirMathhman <sirmathhman@gmail.com>
2026-08-09 22:39:16 -0400

    Add test: parentheses with let declarations should throw

--- index.test.ts
+++ index.test.ts
@@ -50,3 +50,7 @@ test('executeTuff("(2 + 3) * 4") => 20', () => {
 test('executeTuff("{ 2 + 3 } * 4") => 20', () => {
   expect(executeTuff("{ 2 + 3 } * 4")).toBe(20);
 });
+
+test('compileTuff("( let x = 2 + 3; x ) * 4") => Error', () => {
+  expect(() => compileTuffToJS("( let x = 2 + 3; x ) * 4")).toThrow();
+});
