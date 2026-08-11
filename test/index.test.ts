c3478ffc39c6d3bf644e850aea7383e39b7dccca
SirMathhman <sirmathhman@gmail.com>
2026-08-11 19:38:33 -0400

    Add test for nested if/else: if (false) 2 else if (false) 3 else 4 => 4

--- test/index.test.ts
+++ test/index.test.ts
@@ -105,4 +105,10 @@ describe("evaluate", () => {
   it('evaluate("let x = if (false) 2 else 3; x") => 3', () => {
     expect(evaluate("let x = if (false) 2 else 3; x")).toBe(3);
   });
+
+  it('evaluate("let x = if (false) 2 else if (false) 3 else 4; x") => 4', () => {
+    expect(evaluate("let x = if (false) 2 else if (false) 3 else 4; x")).toBe(
+      4,
+    );
+  });
 });
