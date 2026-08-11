889e9e7890609b89e702d1a111135a318d78201d
SirMathhman <sirmathhman@gmail.com>
2026-08-11 15:15:04 -0400

    evaluate parses plain numbers

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,12 @@
 import { describe, expect, it } from "bun:test";
 import { evaluate } from "./index";
 
-describe("evalute", () => {
-  it('evalute("") => 0', () => {
+describe("evaluate", () => {
+  it('evaluate("") => 0', () => {
     expect(evaluate("")).toBe(0);
+  });
+
+  it('evaluate("1") => 1', () => {
+    expect(evaluate("1")).toBe(1);
   });
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export function evaluate(source: string): number {
   if (source === "") return 0;
+  const num = Number(source);
+  if (source.trim() === String(num)) return num;
   throw new Error("Invalid source: " + source);
 }
