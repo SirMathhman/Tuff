88a9da86ffcaa1773b66900125fdba2c05c631be
SirMathhman <sirmathhman@gmail.com>
2026-08-11 10:45:35 -0400

    test: add let expression in braces (accidental match)

--- index.test.ts
+++ index.test.ts
@@ -49,4 +49,8 @@ describe("evaluate", () => {
   it('evaluate("{ 2 + 3 } * 4") => 20', () => {
     expect(evaluate("{ 2 + 3 } * 4")).toBe(20);
   });
+
+  it('evaluate("{ let x = 2 + 3; x } * 4") => 20', () => {
+    expect(evaluate("{ let x = 2 + 3; x } * 4")).toBe(20);
+  });
 });
