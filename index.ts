ba2b7dc66da70434d67e320cde9f73a8391c070e
SirMathhman <sirmathhman@gmail.com>
2026-08-11 15:16:08 -0400

    evaluate handles addition expressions

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 export function evaluate(source: string): number {
   if (source === "") return 0;
-  const num = Number(source);
-  if (source.trim() === String(num)) return num;
-  throw new Error("Invalid source: " + source);
+  const parts = source.split("+");
+  if (parts.length === 1) {
+    const num = Number(parts[0]!);
+    if (parts[0]!.trim() === String(num)) return num;
+    throw new Error("Invalid source: " + source);
+  }
+  const result = parts.map(p => Number(p.trim())).reduce((a, b) => a + b, 0);
+  return result;
 }
