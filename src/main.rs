0a0ed4c2eb904b5a3fa3b16e6329101e100bfc4a
SirMathhman <sirmathhman@gmail.com>
2026-08-11 00:10:51 -0400

    Add test case: evaluate("1") => 1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ fn main() {
 }
 
 #[allow(dead_code)]
-fn evaluate(_input: &str) -> i64 {
-    0
+fn evaluate(input: &str) -> i64 {
+    if input.is_empty() {
+        0
+    } else {
+        input.parse().unwrap_or(0)
+    }
 }
 
 #[cfg(test)]
@@ -14,5 +18,10 @@ mod tests {
     #[test]
     fn test_evaluate_empty_string() {
         assert_eq!(evaluate(""), 0);
+    }
+
+    #[test]
+    fn test_evaluate_one() {
+        assert_eq!(evaluate("1"), 1);
     }
 }
