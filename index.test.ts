8462e66b990b8375573f8ed1b446ba2bf6626e89
SirMathhman <sirmathhman@gmail.com>
2026-08-10 23:52:02 -0400

    Scaffold

---
+++ index.test.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "bun:test";
+import { CompileErrorType, compileTuffToJS } from ".";
+
+function expectValid(
+  tuffSource: string,
+  expectedExitCode: number,
+  args: string[] = [],
+) {
+  const generatedJSResult = compileTuffToJS(tuffSource);
+  if (!generatedJSResult.isOk) {
+    throw new Error(generatedJSResult.value.message);
+  }
+
+  const generatedJS = generatedJSResult.value;
+
+  try {
+    let actualExitCode = 0;
+    let process = {
+      exit(code: number) {
+        actualExitCode = code;
+      },
+    };
+
+    new Function("process", "args", generatedJS)(process, args);
+
+    if (expectedExitCode !== actualExitCode) {
+      throw new Error(
+        "Expected exit code '" +
+          expectedExitCode +
+          "' but was actually '" +
+          actualExitCode +
+          "'. Generated: " +
+          generatedJS,
+      );
+    }
+  } catch (e) {
+    throw new Error("Failed to execute generated JS: '" + generatedJS + "'");
+  }
+}
+
+function expectInvalid(tuffSource: string, expectedType: CompileErrorType) {
+  const result = compileTuffToJS(tuffSource);
+  if (result.isOk) {
+    throw new Error(
+      "Expected a compilation failure but generated: '" + result.value + "'",
+    );
+  }
+
+  if (result.value.type !== expectedType) {
+    throw new Error(
+      "Unexpected error: " + result.value.type + " - " + result.value.message,
+    );
+  }
+}
---
+++ index.ts
@@ -0,0 +1,32 @@
+export interface Ok<T> {
+  isOk: true;
+  value: T;
+}
+
+export interface Err<X> {
+  isOk: false;
+  value: X;
+}
+
+export type Result<T, X> = Ok<T> | Err<X>;
+
+export enum CompileErrorType {
+  Invalid,
+}
+
+export interface CompileError {
+  type: CompileErrorType;
+  message: string;
+}
+
+export function compileTuffToJS(
+  tuffSource: string,
+): Result<string, CompileError> {
+  return {
+    isOk: false,
+    value: {
+      type: CompileErrorType.Invalid,
+      message: "Invalid source: " + tuffSource,
+    },
+  };
+}
