27db38eae0b5efd084d9e8b1d797e952330fe62b
SirMathhman <sirmathhman@gmail.com>
2026-08-14 23:24:12 -0400

    Remove

--- index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "bun:test";
-import { evaluate } from "./index.ts";
-
-describe("evaluate", () => {
-  test('evaluate("") returns 0', () => {
-    expect(evaluate("")).toBe(0);
-  });
-});
