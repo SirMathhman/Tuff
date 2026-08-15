c868a214e2f5064169c99b54a09678ee34dd3a38
SirMathhman <sirmathhman@gmail.com>
2026-08-15 02:29:28 -0400

    test: add chained addition case

--- index.test.ts
+++ index.test.ts
@@ -16,3 +16,7 @@ test('interpret("abc") throws', () => {
 test('interpret("1 + 2") => 3', () => {
   expect(interpret("1 + 2")).toBe(3);
 });
+
+test('interpret("1 + 2 + 3") => 6', () => {
+  expect(interpret("1 + 2 + 3")).toBe(6);
+});
