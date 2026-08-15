567a5cd39f6359505feac8244c4559eaba2a47b7
SirMathhman <sirmathhman@gmail.com>
2026-08-15 02:25:02 -0400

    Add test: interpret('1') => 1

--- index.test.ts
+++ index.test.ts
@@ -4,3 +4,7 @@ import { interpret } from "./index.ts";
 test('interpret("") => 0', () => {
   expect(interpret("")).toBe(0);
 });
+
+test('interpret("1") => 1', () => {
+  expect(interpret("1")).toBe(1);
+});
