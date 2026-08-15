936c5bfa05eefe887e7b75168a7aa00dcd0dbfcf
SirMathhman <sirmathhman@gmail.com>
2026-08-15 02:46:24 -0400

    test: cover all error paths and operators for 100% coverage

--- index.test.ts
+++ index.test.ts
@@ -24,3 +24,31 @@ test('interpret("1 + 2 + 3") => 6', () => {
 test('interpret("(2 + 3) * 4") => 20', () => {
   expect(interpret("(2 + 3) * 4")).toBe(20);
 });
+
+test('interpret("5 - 3") => 2', () => {
+  expect(interpret("5 - 3")).toBe(2);
+});
+
+test('interpret("8 / 2") => 4', () => {
+  expect(interpret("8 / 2")).toBe(4);
+});
+
+test('interpret("-1") => -1', () => {
+  expect(interpret("-1")).toBe(-1);
+});
+
+test('interpret("1 +") throws', () => {
+  expect(() => interpret("1 +")).toThrow();
+});
+
+test('interpret("1 2") throws', () => {
+  expect(() => interpret("1 2")).toThrow();
+});
+
+test('interpret("(1 2)") throws', () => {
+  expect(() => interpret("(1 2)")).toThrow();
+});
+
+test('interpret(")") throws', () => {
+  expect(() => interpret(")")).toThrow();
+});
