003147d21d7ad613339bb8094da1f1438d372e03
SirMathhman <sirmathhman@gmail.com>
2026-08-17 21:03:13 -0400

    Remove type literal usage

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,13 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(...tseslint.configs.recommended, {
   ignores: ["node_modules/"],
+  rules: {
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TSTypeLiteral",
+        message: "Use a named interface instead.",
+      },
+    ],
+  },
 });
