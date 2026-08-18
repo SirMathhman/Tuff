5edffcddca3ad22d6d9186b011a2b5af64a7ca3a
SirMathhman <sirmathhman@gmail.com>
2026-08-18 12:46:34 -0400

    Add max lines controls

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,22 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
   ignores: ["coverage/"],
+  rules: {
+    "max-lines": [
+      "warn",
+      {
+        max: 300,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    "max-lines-per-function": [
+      "warn",
+      {
+        max: 50,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+  },
 });
