a9295b9b01da419dc932c8388b598e4263f0d2ae
SirMathhman <sirmathhman@gmail.com>
2026-08-18 12:58:13 -0400

    Add test case: evaluate("2 + 3 * 4") => 14

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,6 +25,10 @@ describe("evaluate", () => {
     expect(evaluate("2 * 3 + 4")).toEqual({ ok: true, value: 10 });
   });
 
+  it('returns 14 for the input "2 + 3 * 4"', () => {
+    expect(evaluate("2 + 3 * 4")).toEqual({ ok: true, value: 14 });
+  });
+
   it('returns 24 for the input "2 * 3 * 4"', () => {
     expect(evaluate("2 * 3 * 4")).toEqual({ ok: true, value: 24 });
   });
