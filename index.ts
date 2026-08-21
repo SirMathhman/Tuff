ce1c2bfe056476bb272365c90874fa35672ad498
SirMathhman <sirmathhman@gmail.com>
2026-08-21 00:34:56 -0400

    Add test case evaluate('1 + 2') => 3 and support addition in evaluate

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export function evaluate(input: string): number {
   if (input === "") return 0;
-  return Number(input);
+  return input
+    .split("+")
+    .reduce((sum, part) => sum + Number(part.trim()), 0);
 }
