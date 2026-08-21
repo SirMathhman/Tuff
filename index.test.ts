81a17e7fc83be84318766a7257eee6eea5bb0834
SirMathhman <sirmathhman@gmail.com>
2026-08-21 13:18:17 -0400

    Add evaluate stub and test for empty string

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,11 @@
 import { expect, test } from "bun:test";
+import { evaluate } from "./index.ts";
 
 test("index runs without throwing", async () => {
   await import("./index.ts");
   expect(true).toBe(true);
+});
+
+test('evaluate("") => 0', () => {
+  expect(evaluate("")).toBe(0);
 });
