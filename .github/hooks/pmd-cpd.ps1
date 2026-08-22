e7f739c141325885118ec426c85c1bacc4566ac3
SirMathhman <sirmathhman@gmail.com>
2026-08-22 14:25:48 -0400

    Ignore

--- .github/hooks/pmd-cpd.ps1
+++ .github/hooks/pmd-cpd.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop'
 
 # Detect duplicated code blocks in Rust sources using PMD CPD.
-pmd cpd --language rust --minimum-tokens 100 --dir src
+pmd cpd --language rust --minimum-tokens 100 --dir src --ignore-identifiers --ignore-literals
 if ($LASTEXITCODE -ne 0) {
     Write-Error "PMD CPD found duplicated code in src/"
     exit 1
