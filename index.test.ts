343d6a497d2b56e2d0c7c3dde50ebe22c1f503eb
SirMathhman <sirmathhman@gmail.com>
2026-08-24 17:42:15 -0400

    Add test for let declaration in evaluate

--- index.test.ts
+++ index.test.ts
@@ -9,4 +9,8 @@ describe("evaluate", () => {
   test('evaluates "return 1;" to 1', () => {
     expect(evaluate("return 1;")).toBe(1);
   });
+
+  test('evaluates "let x = 1; return x;" to 1', () => {
+    expect(evaluate("let x = 1; return x;")).toBe(1);
+  });
 });
