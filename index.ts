0ad1dc86dd3171c5a4f63c6d5aba1865aa8244d5
SirMathhman <sirmathhman@gmail.com>
2026-08-24 11:48:13 -0400

    Nuke

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { evaluateTuff as evaluate } from "./src/evaluate.ts";
-export type { Result } from "./src/result.ts";
-export { Ok, Err, map, andThen, unwrapOr } from "./src/result.ts";
-export type { EvalError } from "./src/errors.ts";
--- src/ast/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./expr.ts";
-export * from "./statement.ts";
