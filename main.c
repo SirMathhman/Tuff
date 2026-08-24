6262e364c9d3cfc0a8dddd6ee2ff962553c8f877
SirMathhman <sirmathhman@gmail.com>
2026-08-24 13:15:38 -0400

    Add evaluate_tuff stub and test: empty string => 0

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int evaluate_tuff(const char *s)
+{
+    (void)s;
+    return 0;
+}
+
 int main()
 {
-        return 0;
-        
+    int failures = 0;
+
+    if (evaluate_tuff("") != 0)
+    {
+        printf("FAIL: evaluate_tuff(\"\") => %d, expected 0\n", evaluate_tuff(""));
+        failures++;
+    }
+
+    if (failures == 0)
+        printf("All tests passed\n");
+
+    return failures != 0;
 }
