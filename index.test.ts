07a8d36121d907e0b32d1dbeeb00099b963ae1ef
SirMathhman <sirmathhman@gmail.com>
2026-08-26 09:33:33 -0400

    Fix indentation

--- index.test.ts
+++ index.test.ts
@@ -16,66 +16,66 @@ function expectUnidentifiedIdentifier(
   }
 }
 
-  test('evaluateTuff("") => 0', () => {
-    expect(evaluateTuff("")).toEqual({ ok: true, value: 0 });
-  });
+test('evaluateTuff("") => 0', () => {
+  expect(evaluateTuff("")).toEqual({ ok: true, value: 0 });
+});
 
-  test('evaluateTuff("return 1;") => 1', () => {
-    expect(evaluateTuff("return 1;")).toEqual({ ok: true, value: 1 });
-  });
+test('evaluateTuff("return 1;") => 1', () => {
+  expect(evaluateTuff("return 1;")).toEqual({ ok: true, value: 1 });
+});
 
-  test('evaluateTuff("let x = 1; return x;") => 1', () => {
-    expect(evaluateTuff("let x = 1; return x;")).toEqual({
-      ok: true,
-      value: 1,
-    });
+test('evaluateTuff("let x = 1; return x;") => 1', () => {
+  expect(evaluateTuff("let x = 1; return x;")).toEqual({
+    ok: true,
+    value: 1,
   });
+});
 
-  test('evaluateTuff("let x = 1; let y = x; return y;") => 1', () => {
-    expect(evaluateTuff("let x = 1; let y = x; return y;")).toEqual({
-      ok: true,
-      value: 1,
-    });
+test('evaluateTuff("let x = 1; let y = x; return y;") => 1', () => {
+  expect(evaluateTuff("let x = 1; let y = x; return y;")).toEqual({
+    ok: true,
+    value: 1,
   });
+});
 
-  test('evaluateTuff("let x = 1; let y = 2; return y;") => 2', () => {
-    expect(evaluateTuff("let x = 1; let y = 2; return y;")).toEqual({
-      ok: true,
-      value: 2,
-    });
+test('evaluateTuff("let x = 1; let y = 2; return y;") => 2', () => {
+  expect(evaluateTuff("let x = 1; let y = 2; return y;")).toEqual({
+    ok: true,
+    value: 2,
   });
+});
 
-  test('evaluateTuff("return unidentifiedIdentifier;") => Err', () => {
-    expectUnidentifiedIdentifier(
-      evaluateTuff("return unidentifiedIdentifier;"),
-      "unidentifiedIdentifier",
-      1,
-    );
-  });
+test('evaluateTuff("return unidentifiedIdentifier;") => Err', () => {
+  expectUnidentifiedIdentifier(
+    evaluateTuff("return unidentifiedIdentifier;"),
+    "unidentifiedIdentifier",
+    1,
+  );
+});
 
-  test('evaluateTuff("let x = missing; return x;") => Err', () => {
-    expectUnidentifiedIdentifier(
-      evaluateTuff("let x = missing; return x;"),
-      "missing",
-      1,
-    );
-  });
+test('evaluateTuff("let x = missing; return x;") => Err', () => {
+  expectUnidentifiedIdentifier(
+    evaluateTuff("let x = missing; return x;"),
+    "missing",
+    1,
+  );
+});
 
-  test('evaluateTuff("let x = 100;") => 0', () => {
-    expect(evaluateTuff("let x = 100;")).toEqual({ ok: true, value: 0 });
-  });
+test('evaluateTuff("let x = 100;") => 0', () => {
+  expect(evaluateTuff("let x = 100;")).toEqual({ ok: true, value: 0 });
+});
 
-  test('evaluateTuff("let mut x = 0; x = 1; return x;") => 1', () => {
-    expect(evaluateTuff("let mut x = 0; x = 1; return x;")).toEqual({
-      ok: true,
-      value: 1,
-    });
+test('evaluateTuff("let mut x = 0; x = 1; return x;") => 1', () => {
+  expect(evaluateTuff("let mut x = 0; x = 1; return x;")).toEqual({
+    ok: true,
+    value: 1,
   });
+});
 
-  test('evaluateTuff("let x = unidentifiedIdentifier;") => Err', () => {
-    expectUnidentifiedIdentifier(
-      evaluateTuff("let x = unidentifiedIdentifier;"),
-      "unidentifiedIdentifier",
-      1,
-    );
-  });
+test('evaluateTuff("let x = unidentifiedIdentifier;") => Err', () => {
+  expectUnidentifiedIdentifier(
+    evaluateTuff("let x = unidentifiedIdentifier;"),
+    "unidentifiedIdentifier",
+    1,
+  );
+});
