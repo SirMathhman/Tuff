5298a67678465c413fc5edd5bae4d6de17fcf975
SirMathhman <sirmathhman@gmail.com>
2026-08-30 21:33:56 -0400

    Add test: evaluate("2 + 3 * 4") => 14

--- index.test.ts
+++ index.test.ts
@@ -32,3 +32,7 @@ test('evaluate("   ") => 0', () => {
 test('evaluate("2 * 3 + 4") => 10', () => {
   expect(evaluate("2 * 3 + 4")).toEqual({ ok: true, value: 10 });
 });
+
+test('evaluate("2 + 3 * 4") => 14', () => {
+  expect(evaluate("2 + 3 * 4")).toEqual({ ok: true, value: 14 });
+});
