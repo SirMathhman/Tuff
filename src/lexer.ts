35ed1c74567d24a579c8d04f14b2a4d81f25c981
SirMathhman <sirmathhman@gmail.com>
2026-08-30 21:18:00 -0400

    Add typecheck script; fix strict index access in lexer

--- src/lexer.ts
+++ src/lexer.ts
@@ -12,14 +12,14 @@ export function lex(input: string): LexResult {
   const tokens: Token[] = [];
   let i = 0;
   while (i < input.length) {
-    const ch = input[i];
+    const ch = input.charAt(i);
     if (/\s/.test(ch)) {
       i++;
       continue;
     }
     if (/[0-9]/.test(ch)) {
       const start = i;
-      while (i < input.length && /[0-9]/.test(input[i])) i++;
+      while (i < input.length && /[0-9]/.test(input.charAt(i))) i++;
       tokens.push({
         type: "number",
         value: Number(input.slice(start, i)),
