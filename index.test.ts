54c1a7eefd2a1a418b6765acbaeeb155db674ce5
SirMathhman <sirmathhman@gmail.com>
2026-08-30 21:10:21 -0400

    Empty input evaluates to 0 (ok result)

--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,6 @@
 import { test, expect } from "bun:test";
 import { evaluate } from "./index.ts";
 
-test('evaluate("") => error', () => {
-  expect(evaluate("")).toEqual({
-    ok: false,
-    error: { kind: "empty", message: "empty input", position: 0 },
-  });
+test('evaluate("") => 0', () => {
+  expect(evaluate("")).toEqual({ ok: true, value: 0 });
 });
