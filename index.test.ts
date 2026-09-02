83a420e3843c978273e2bf3c3a3b5d105d188c1f
SirMathhman <sirmathhman@gmail.com>
2026-09-01 21:11:54 -0400

    Add empty source test case; update architecture doc to match compiler

--- index.test.ts
+++ index.test.ts
@@ -2,25 +2,37 @@ import { test, expect } from "bun:test";
 import { compileTuffToTypeScript } from ".";
 import ts from "typescript";
 
-export function expectValid(tuffSource : string, expectedExitCode : number, args : string[] = []) {
+export function expectValid(
+  tuffSource: string,
+  expectedExitCode: number,
+  args: string[] = [],
+) {
+  const compiledTS = compileTuffToTypeScript(tuffSource);
+  const compiledJS = ts.transpile(compiledTS);
+
+  let actualExitCode = 0;
+  let process = {
+    exit(code: number) {
+      actualExitCode = code;
+    },
+  };
+
+  new Function("process", "args", compiledJS)(process, [
+    "mock_program_name",
+    ...args,
+  ]);
+  expect(actualExitCode).toBe(expectedExitCode);
+}
+
+export function expectInvalid(tuffSource: string) {
+  try {
     const compiledTS = compileTuffToTypeScript(tuffSource);
-    const compiledJS = ts.transpile(compiledTS);
-
-    let actualExitCode = 0;
-    let process = {
-        exit(code : number) {
-            actualExitCode = code;
-        }
-    }
-
-    new Function("process", "args", compiledJS)(process, ["mock_program_name",  ...args]);
-    expect(actualExitCode).toBe(expectedExitCode);
+    throw new Error(
+      "Expected test to fail but compiled to: '" + compiledTS + "'",
+    );
+  } catch (e) {}
 }
 
-export function expectInvalid(tuffSource : string) {
-    try {
-        const compiledTS =         compileTuffToTypeScript(tuffSource);
-        throw new Error("Expected test to fail but compiled to: '" + compiledTS + "'")
-    } catch(e) {
-    }
-}
+test("empty source", () => {
+  expectValid("", 0, []);
+});
